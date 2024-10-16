a102e1469d244de34d6996f5e130df7c9182101f
Одоевский <Odoevskiy.PE@gazprom-neft.ru>
2024-10-16 13:48:48 +0300

    fix

--- src/main/java/itmo_diploma/config/OpenApi30.java
+++ src/main/java/itmo_diploma/config/OpenApi30.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
-// http://localhost:8080/swagger-ui/index.html#/
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "ITMO_DIPLOMA", version = "1.0"))
 public class OpenApi30 {
